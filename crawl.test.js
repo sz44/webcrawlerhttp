0063dd1d01a2480e43eb39abecca15216064069a
sz44 <semion44@gmail.com>
2023-12-25 18:17:26 -0800

    added 2 more normalizeURL tests

--- crawl.test.js
+++ crawl.test.js
@@ -2,8 +2,22 @@ const { normalizeURL } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
-  const input = "https://boot.dev.blog/path/";
+  const input = "https://blog.boot.dev/path/";
   const actual = normalizeURL(input);
-  const expected = "boot.dev.blog/path";
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http/s", () => {
+  const input = "http://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
   expect(actual).toEqual(expected);
 });
